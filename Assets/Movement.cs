4252cda24114e364bad431fd516bd9b4fb8ab0e3
VolodymyrBrahar <vbrahar@edu.cdv.pl>
2024-06-13 19:36:26 +0200

    Base of the game done

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D rigidbody2D;
+    public KeyCode UpKey;
+    public KeyCode DownKey;
+    public float speed = 2;
 
-    // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -15,19 +17,20 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(UpKey) && transform.position.y < 4)
         {
-            rigidbody2D.velocity = Vector2.up;
+            rigidbody2D.velocity = Vector2.up * speed;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(DownKey) && transform.position.y > -4)
         {
-            rigidbody2D.velocity = Vector2.down;
+            rigidbody2D.velocity = Vector2.down * speed;
         }
         else
         {
             rigidbody2D.velocity = Vector2.zero;
         }
     }
+
     private void OnMouseDown()
     {
         GetComponent<SpriteRenderer>().color = Color.red;
